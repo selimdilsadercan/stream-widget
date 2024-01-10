0db855865677817f7bf2876634102591332ecb3c
selimdilsad <selimdilsadercan@gmail.com>
2024-01-10 22:44:02 +0300

    feat: added icons that changes visible

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { User, User2 } from "lucide-react";
+
 function Page() {
-  return <div className="text-2xl text-amber-900 hover:text-blue-900">Naber</div>;
+  return (
+    <div className="flex flex-col items-start justify-start w-[200px] h-[200px] group">
+      <div className="text-2x">Puan Tablosu</div>
+      <User2 className="hidden group-hover:block" />
+      <div className="text-2x">zSuat</div>
+      <div className="text-2x">OoSee</div>
+      <div className="text-2x">Mr. C</div>
+      <div className="text-2x">BRkyAytr</div>
+      <div className="text-2x">YYYMuh</div>
+      <div className="text-2x">Hilmi</div>
+    </div>
+  );
 }
 
 export default Page;
